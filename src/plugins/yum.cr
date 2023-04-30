c470689fdd2846cf55e99b5e00cddf92a2edaef4
Lampros Chaidas <lampros@chaidas.com>
2023-04-30 15:04:41 -0700

    Changing the yum.cr file from a script to a module/function

--- src/plugins/yum.cr
+++ src/plugins/yum.cr
@@ -1,94 +1,85 @@
 require "../common_functions.cr"
 
-unless command_in_our_path?("yum")
-  abort "yum not found"
-end
+module YUM
+  extend self
 
-if ARGV.size.zero?
-  abort "no arguments given"
-end
+  unless command_in_our_path?("yum")
+    abort "yum not found"
+  end
 
-if ARGV.size != 2
-  abort "need 2 arguments"
-end
-
-# returns true if the package is already installed
-# returns false if the package is not installed
-def check_if_yum_package_is_installed(name_of_package)
-  exit_code, command_output = run_cmd("yum list installed #{name_of_package}")
-  return exit_code, command_output
-end
-
-def get_real_yum_package_name(name_of_package)
-  exit_code, command_output = run_cmd("yum provides #{name_of_package}")
-  found_package_name="none"
-  if exit_code.zero?
-    # we convert the string to an array by splitting on new lines
-    command_output = command_output.split("\n")
-    # any empty lines should be removed as well as lines that we don't need from the array
-    command_output.reject!{ |line| line.empty? || line.starts_with?("Repo") || line.starts_with?("Matched from:") || line.starts_with?("Provide") || line.starts_with?("Last metadata expiration check") }
-    # what's left should be the package name - it will look like this:
-    # "mysql-8.0.30-1.module_el8.6.0+3340+d764b636.x86_64 : MySQL client programs and shared libraries"
-    # so we split on " : " and keep only the first part which is the real package name
-    result = command_output.first.split(" : ")[0]
-    return exit_code, result
-  else
+  # returns true if the package is already installed
+  # returns false if the package is not installed
+  def check_if_yum_package_is_installed(name_of_package)
+    exit_code, command_output = run_cmd("yum list installed #{name_of_package}")
     return exit_code, command_output
   end
-end
 
-def install_yum_package(name_of_package)
-  exit_code, command_output = run_cmd("yum --assumeyes install #{name_of_package}")
-  return exit_code, command_output  
-end
-
-def remove_yum_package(name_of_package)
-  exit_code, command_output = run_cmd("yum --assumeyes remove #{name_of_package}")
-  return exit_code, command_output
-end
-
-if ARGV[0] == "install" && !ARGV[1].nil?
-  package_name = ARGV[1]
-  exit_code, package_name = get_real_yum_package_name(package_name)
-  if exit_code.zero?
-    puts "Using package name: \"#{package_name}\""
-  else
-    abort "Cannot find package: \"#{package_name.delete('\n')}\""
-  end
-  if check_if_yum_package_is_installed(package_name)[0].zero?
-    puts "package \"#{package_name}\": already installed"
-  else
-    puts "package \"#{package_name}\": Not installed"
-    exit_code, command_output = install_yum_package(package_name)
+  def get_real_yum_package_name(name_of_package)
+    exit_code, command_output = run_cmd("yum provides #{name_of_package}")
     if exit_code.zero?
-      puts "package \"#{package_name}\": Successfully installed"
+      # we convert the string to an array by splitting on new lines
+      command_output = command_output.split("\n")
+      # any empty lines should be removed as well as lines that we don't need from the array
+      command_output.reject!{ |line| line.empty? || line.starts_with?("Repo") || line.starts_with?("Matched from:") || line.starts_with?("Provide") || line.starts_with?("Last metadata expiration check") }
+      # what's left should be the package name - it will look like this:
+      # "mysql-8.0.30-1.module_el8.6.0+3340+d764b636.x86_64 : MySQL client programs and shared libraries"
+      # so we split on " : " and keep only the first part which is the real package name
+      result = command_output.first.split(" : ")[0]
+      return exit_code, result
     else
-      abort "package \"#{package_name}\": Could not install: Error: #{command_output}"
+      return exit_code, command_output
     end
   end
-end
 
-if ARGV[0] == "absent" || ARGV[0] == "removed" && !ARGV[1].nil?
-  package_name = ARGV[1]
-  exit_code, package_name = get_real_yum_package_name(package_name)
-  if exit_code.zero?
-    puts "Using package name: \"#{package_name}\""
-  else
-    abort "Cannot find package: \"#{package_name.delete('\n')}\""
+  def install_yum_package(name_of_package)
+    exit_code, command_output = run_cmd("yum --assumeyes install #{name_of_package}")
+    return exit_code, command_output
   end
-  if !check_if_yum_package_is_installed(package_name)[0].zero?
-    puts "package \"#{package_name}\": Not installed"
-    exit 0
-  else
-    puts "package \"#{package_name}\": already installed"
-    exit_code, command_output = remove_yum_package(package_name)
+
+  def remove_yum_package(name_of_package)
+    exit_code, command_output = run_cmd("yum --assumeyes remove #{name_of_package}")
+    return exit_code, command_output
+  end
+
+  def install_yum_package(package_name)
+    exit_code, package_name = get_real_yum_package_name(package_name)
     if exit_code.zero?
-      puts "package \"#{package_name}\": Successfully removed"
+      puts "Using package name: \"#{package_name}\""
     else
-      abort "package \"#{package_name}\": Could not remove: Error: #{command_output}"
+      abort "Cannot find package: \"#{package_name.delete('\n')}\""
+    end
+    if check_if_yum_package_is_installed(package_name)[0].zero?
+      puts "package \"#{package_name}\": already installed"
+    else
+      puts "package \"#{package_name}\": Not installed"
+      exit_code, command_output = install_yum_package(package_name)
+      if exit_code.zero?
+        puts "package \"#{package_name}\": Successfully installed"
+      else
+        abort "package \"#{package_name}\": Could not install: Error: #{command_output}"
+      end
     end
   end
-end
 
-# if you got here then it's an unsupported switch
-abort "Unsupported switch: \"#{ARGV[0]}\" - bailing."
+  def remove_yum_package(package_name)
+    exit_code, package_name = get_real_yum_package_name(package_name)
+    if exit_code.zero?
+      puts "Using package name: \"#{package_name}\""
+    else
+      abort "Cannot find package: \"#{package_name.delete('\n')}\""
+    end
+    if !check_if_yum_package_is_installed(package_name)[0].zero?
+      puts "package \"#{package_name}\": Not installed"
+      exit 0
+    else
+      puts "package \"#{package_name}\": already installed"
+      exit_code, command_output = remove_yum_package(package_name)
+      if exit_code.zero?
+        puts "package \"#{package_name}\": Successfully removed"
+      else
+        abort "package \"#{package_name}\": Could not remove: Error: #{command_output}"
+      end
+    end
+  end
+
+end
